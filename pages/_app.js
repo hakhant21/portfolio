72a3966ddb094be9bed9b7a26d31273004b25cd0
hakkhant <hakhant27@gmail.com>
2022-08-17 16:21:07 +0630

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Header from '../components/shared/Header';
 import Footer from '../components/shared/Footer';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 import '../styles/tailwind.css';
 import '../styles/globals.css';
@@ -10,10 +9,6 @@ function MyApp({ Component, pageProps }) {
     <div className='max-w-6xl mx-auto'>
       <Header />
       <Component {...pageProps} />
-      <MessengerCustomerChat
-        pageId={process.env.REACT_APP_ID}
-        appId={process.env.REACT_PAGE_ID}
-      />
       <Footer />
     </div>
   );

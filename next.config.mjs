b4af67a602e36490318012767f2fd2aa5a1e79f3
Htet Aung Khant <hakhant21@gmail.com>
2025-06-27 01:07:08 +0630

    .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        SENDER_EMAIL: process.env.SENDER_EMAIL,
+        RECIPIENT_EMAIL: process.env.RECIPIENT_EMAIL,
+        MAILTRAP_TOKEN: process.env.MAILTRAP_TOKEN
+    }
+};
 
 export default nextConfig;
